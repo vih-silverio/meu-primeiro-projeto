ee2dabe8a6970357899bdb271bde4def188f54fb
vih-silverio <vitoryabarbosa704@gmail.com>
2025-10-21 10:41:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Rolagem suave ao clicar nos links do menu
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
